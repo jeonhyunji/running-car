335f1e627314f6ca0c61cef3ee412dda7811d2a6
jeonhyunji <94jeonhyunji@gmail.com>
2020-09-16 21:12:00 +0900

    delete commit:

--- src/racing_game.py
+++ src/racing_game.py
@@ -4,19 +4,26 @@ import math
 from math import sin, radians, degrees, copysign
 from pygame.math import Vector2
 
-SCREEN_WIDTH = 1000
-SCRENN_HEIGHT = 800
-
-IMAGE_PATH = "resources/images/greencar.png"
-TRACK_IMAGE_PATH = "resources/images/track.png"
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN= (0, 255, 0)
 RED  = (255, 0, 0)
 YELLOW = (255, 255, 0)
+GRAY = (169, 169, 169)
 
-CAR_WIDTH = 60
-CAR_HEIGHT = 30
+BACKGROUND_COLOR = GRAY
+TEXT_COLOR = WHITE
+
+IMAGE_PATH = "resources/images/car_green.png"
+TRACK_IMAGE_PATH = "resources/images/track2.png"
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+# CAR_WIDTH = 60
+# CAR_HEIGHT = 30
+
+PPU = 32 # pixel per unit?
 
 class Car:
     # length is car length
@@ -49,42 +56,35 @@ class Car:
         self.position += self.velocity.rotate(-self.angle) * dt
         self.angle += degrees(angular_velocity) * dt
 
-# car, track image load
-track_image = pygame.image.load(TRACK_IMAGE_PATH)
-car_image = pygame.image.load(IMAGE_PATH)
-
-# create a mask for each of them.
-track_mask = pygame.mask.from_surface(track_image, 50)
-car_mask = pygame.mask.from_surface(car_image, 50)
-
-# this is where the car, track are.
-track_rect = track_image.get_rect()
-car_rect = car_image.get_rect()
-
-mask = pygame.mask.from_surface(track_image)
-mask_fx = pygame.mask.from_surface(pygame.transform.flip(track_image, True, False))
-mask_fy = pygame.mask.from_surface(pygame.transform.flip(track_image, False, True))
-mask_fx_fy = pygame.mask.from_surface(pygame.transform.flip(track_image, True, True))
-flipped_masks = [[mask, mask_fy], [mask_fx, mask_fx_fy]]
-
-beam_surface = pygame.Surface((250, 250), pygame.SRCALPHA)
-
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Car tutorial")
 
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCRENN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.ticks = 60
         self.exit = False
         self.alive = True
 
+        # car, track image load
+        self.track_image = pygame.image.load(TRACK_IMAGE_PATH)
+        self.car_image = pygame.image.load(IMAGE_PATH)
+
+        # create a mask for each of them.
+        self.track_mask = pygame.mask.from_surface(self.track_image, 50)
+        self.car_mask = pygame.mask.from_surface(self.car_image, 50)
+
+        mask = pygame.mask.from_surface(self.track_image)
+        mask_fx = pygame.mask.from_surface(pygame.transform.flip(self.track_image, True, False))
+        mask_fy = pygame.mask.from_surface(pygame.transform.flip(self.track_image, False, True))
+        mask_fx_fy = pygame.mask.from_surface(pygame.transform.flip(self.track_image, True, True))
+        self.flipped_masks = [[mask, mask_fy], [mask_fx, mask_fx_fy]]
+
+        self.beam_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
+
     def run(self):
-        # car_image = pygame.image.load(IMAGE_PATH)
-        # car = Car(10, 10)
         car = Car(4, 8)
-        ppu = 32
 
         while not self.exit:
             dt = self.clock.get_time() / 1000
@@ -93,7 +93,7 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     print("pygame quit")
-                    pygame.quit()
+                    self.exit = True
             
             if (self.alive):
                 # User input
@@ -137,49 +137,54 @@ class Game:
                 car.update(dt)
 
             # get new position from rotated rectangular object
-            rotated = pygame.transform.rotate(car_image, car.angle)
+            rotated = pygame.transform.rotate(self.car_image, car.angle)
             rect = rotated.get_rect()
-            carNewPosition = car.position * ppu - (rect.width / 2, rect.height / 2)
+            carNewPosition = car.position * PPU - (rect.width / 2, rect.height / 2)
 
             # reset screen
-            self.screen.fill(WHITE)
-
-            # draw
-            # for angle in range(0, 359, 30):
-            for angle in range(-45, 135, 45):
-                self.draw_beam(angle, carNewPosition)
-
-            # check crash, if crash draw yellow rectangular ! and don't move!
-            isCrash = self.checkCrash(carNewPosition)
-            if (isCrash):
-                self.alive = False
-                self.drawCrash(rotated, carNewPosition)
-
-            # print car infomation
-            self.printText("car position: " + str(car.position), 10, 10)
-            self.printText("car new position: " + str(carNewPosition), 10, 40)
-            self.printText("acceleration: " + str(car.acceleration), 10, 70)
-            self.printText("velocity: " + str(car.velocity), 10, 100)
-            self.printText("rotate rect: " + str(rect), 10, 130)
+            self.screen.fill(BACKGROUND_COLOR)
 
             # drawing car new position
-            self.screen.blit(rotated, carNewPosition)
-            self.screen.blit(track_image, (0,0))
+            rotatedRect = self.screen.blit(rotated, carNewPosition)
+            self.screen.blit(self.track_image, (0,0))
+
+            # draw beam
+            # for angle in range(-90, 91, 30):
+            #     self.draw_beam(angle, rotatedRect.center)
+            beam_angle_arr = [-90, -45, 0, 45, 90]
+            for angle in beam_angle_arr:
+                beam_angle = angle - car.angle
+                self.draw_beam(beam_angle, rotatedRect.center)
+
+            # check crash, if crash draw yellow rectangular ! and don't move!
+            isCrash = self.check_crash(carNewPosition)
+            if (isCrash):
+                self.alive = False
+                self.draw_crash(rotatedRect, carNewPosition)
+
+            # print car infomation
+            self.print_text("car position: " + str(car.position), 10, 10)
+            self.print_text("car new position: " + str(carNewPosition), 10, 40)
+            self.print_text("acceleration: " + str(car.acceleration), 10, 70)
+            self.print_text("velocity: " + str(car.velocity), 10, 100)
+
             pygame.display.flip()
             self.clock.tick(self.ticks)
 
-    def checkCrash(self, position):
+        pygame.quit()
+
+    def check_crash(self, position):
+        track_rect = self.track_image.get_rect()
         offset_x = position.x - track_rect[0]
         offset_y = position.y - track_rect[1]
-        overlap = track_mask.overlap(car_mask, (int(offset_x), int(offset_y)))
+        overlap = self.track_mask.overlap(self.car_mask, (int(offset_x), int(offset_y)))
         if (overlap):
             return True
         else:
             return False
 
-    def drawCrash(self, rotated, position):
-        self.printText("crash!!", 20, 20)
-        rotatedRect = self.screen.blit(rotated, position)
+    def draw_crash(self, rotatedRect, position):
+        self.print_text("crash!!", position.x, (position.y-20))
         pygame.draw.rect(self.screen, YELLOW, rotatedRect, 2)
 
     def draw_beam(self, angle, pos):
@@ -188,34 +193,34 @@ class Game:
 
         flip_x = c < 0
         flip_y = s < 0
-        filpped_mask = flipped_masks[flip_x][flip_y]
+        filpped_mask = self.flipped_masks[flip_x][flip_y]
 
         # compute beam final point
-        x_dest = 500 * abs(c)
-        y_dest = 500 * abs(s)
+        x_dest = SCREEN_WIDTH * abs(c)
+        y_dest = SCREEN_HEIGHT * abs(s)
 
-        beam_surface.fill((0, 0, 0, 0))
+        self.beam_surface.fill((0, 0, 0, 0))
 
         # draw a single beam to the beam surface based on computed final point
-        pygame.draw.line(beam_surface, BLUE, (0, 0), (x_dest, y_dest))
-        beam_mask = pygame.mask.from_surface(beam_surface)
+        pygame.draw.line(self.beam_surface, BLUE, (0, 0), (x_dest, y_dest))
+        beam_mask = pygame.mask.from_surface(self.beam_surface)
 
         # find overlap between "global mask" and current beam mask
-        offset_x = 499-pos[0] if flip_x else pos[0]
-        offset_y = 499-pos[1] if flip_y else pos[1]
+        offset_x = (SCREEN_WIDTH-1)-pos[0] if flip_x else pos[0]
+        offset_y = (SCREEN_HEIGHT-1)-pos[1] if flip_y else pos[1]
         hit = filpped_mask.overlap(beam_mask, (int(offset_x), int(offset_y)))
         if hit is not None and (hit[0] != pos[0] or hit[1] != pos[1]):
-            hx = 499 - hit[0] if flip_x else hit[0]
-            hy = 499 - hit[1] if flip_y else hit[1]
+            hx = (SCREEN_WIDTH-1) - hit[0] if flip_x else hit[0]
+            hy = (SCREEN_HEIGHT-1) - hit[1] if flip_y else hit[1]
             hit_pos = (hx, hy)
 
             pygame.draw.line(self.screen, BLUE, pos, hit_pos)
-            pygame.draw.circle(self.screen, GREEN, hit_pos, 3)
+            pygame.draw.circle(self.screen, GREEN, hit_pos, 5)
 
-    def printText(self, text, x, y):
+    def print_text(self, text, x, y):
         # font loading, text size: 15
-        fontObj = pygame.font.Font('resources/NanumSquareOTF_acB.otf', 15)
-        printTextObj = fontObj.render(text, True, WHITE)   
+        fontObj = pygame.font.Font('resources/fonts/NanumSquareOTF_acB.otf', 15)
+        printTextObj = fontObj.render(text, True, TEXT_COLOR)   
         printTextRect = printTextObj.get_rect();                     
         printTextRect.topleft = (x, y)                               
         self.screen.blit(printTextObj, printTextRect)    
