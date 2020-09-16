16a9602d408ee6a5bc32bc12422184052db5de36
jeonhyunji <94jeonhyunji@gmail.com>
2020-09-16 21:10:57 +0900

    code refactoring
    :q
    q!

---
+++ src/car.py
@@ -0,0 +1,33 @@
+from pygame.math import Vector2
+import math
+
+class Car:
+    # length is car length
+    # max_steering is 30 degree 
+    # max_acceleration is 5 meters per second
+    def __init__(self, x, y, angle=0.0, length=4, max_steering=100, max_acceleration=5.0):
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(0.0, 0.0)
+        self.angle = angle
+        self.length = length
+        self.max_acceleration = max_acceleration
+        self.max_steering = max_steering
+        self.max_velocity = 20
+        self.brake_deceleration = 10
+        self.free_deceleration = 2
+
+        self.acceleration = 0.0
+        self.steering = 0.0
+
+    def update(self, dt):
+        self.velocity += (self.acceleration * dt, 0)
+        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
+
+        if self.steering:
+            turning_radius = self.length / math.sin(math.radians(self.steering))
+            angular_velocity = self.velocity.x / turning_radius
+        else:
+            angular_velocity = 0
+
+        self.position += self.velocity.rotate(-self.angle) * dt
+        self.angle += math.degrees(angular_velocity) * dt
